7a92e1f32eaabd96f6d5ff4703d42251f95c927e
andrewbarchuk <andrewbarchuk@gmail.com>
2021-03-05 17:58:23 +0200

    up

---
+++ page-home.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Template Name: Home
+*/
+
+/**
+ * 
+ * The template for displaying portfolio
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package ieverly
+ */
+
+get_header();
+?>
+
+this home page
+
+<?php
+get_footer();
